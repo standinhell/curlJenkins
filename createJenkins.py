f75115ac0b5cd37a537448438789bc0a66f688ea
zhaoshenqiao <zsqqfyun@163.com>
2018-12-03 16:20:59 +0800

    '增加参数修改'

--- createJenkins.py
+++ createJenkins.py
@@ -2,7 +2,10 @@
 import jenkins
 
 # 获取jenkins连接
-server = jenkins.Jenkins('http://jenkins.66123123.com', username='', password='')
+server = jenkins.Jenkins('http://jenkins.66123123.com', username='leading2018', password='leading2018')
+
+# 上游项目构建完成后是否构建下游项目
+UPSTREAM_CHANGES = 'true'
 
 # 需要创建的job列表
 jobs_arr = ['authorization-service', 'authorization-service-api', 'configuration-server', 'file-service',
@@ -10,7 +13,8 @@ jobs_arr = ['authorization-service', 'authorization-service-api', 'configuration
             'leading-idempotent-component', 'leading-parent-pom', 'leading-resource-base', 'location-service',
             'location-service-api', 'product-service', 'product-service-api', 'service-api-gateway', 'service-registry',
             'supplier-service', 'supplier-service-api', 'turbine-dashboard', 'user-service', 'user-service-api',
-            'warehouse-service', 'warehouse-service-api']
+            'warehouse-service', 'warehouse-service-api', 'mapping-service-api', 'mapping-service', 'erp-old-service', 'organization-service-api',
+            'organization-service', 'customer-service-api', 'customer-service']
 gitlab_prefix = 'http://gitlab.66123123.com/leading-service/'
 gitlab_suffix = '.git'
 group_id = 'com.leading'
@@ -21,7 +25,7 @@ for jobs_name in jobs_arr:
     if 'datasource' in jobs_name:
         group_id = 'com.leading.db'
 
-    if 'api' in jobs_name or 'leading' in jobs_name:
+    if '-service-api' in jobs_name or 'leading' in jobs_name:
         with open('config-api.xml') as f:
             profile = f.read()
         GOALS = 'clean deploy'
@@ -31,15 +35,18 @@ for jobs_name in jobs_arr:
             .replace("GOALS", GOALS) \
             .replace("SSH_CONFIG_NAME", "235") \
             .replace("SSH_CONFIG_COMMAND", "/home/leading/leading_redeploy.sh " + jobs_name) \
-            .replace("GIT_BRANCH", "feature-initialization")
+            .replace("GIT_BRANCH", "feature-initialization") \
+            .replace("UPSTREAM_CHANGES",UPSTREAM_CHANGES)
+            
 
         JOB_CONFIG_DEV = profile.replace("GITLAB_URL", gitlab_url) \
             .replace("GROUP_ID", group_id) \
             .replace("JOB_NAME", jobs_name) \
             .replace("GOALS", GOALS) \
             .replace("SSH_CONFIG_NAME", "dev") \
-            .replace("SSH_CONFIG_COMMAND", "/home/scm/leading_redeploy.sh " + jobs_name) \
-            .replace("GIT_BRANCH", "feature-initialization")
+            .replace("SSH_CONFIG_COMMAND", "/home/dev/leading_redeploy.sh " + jobs_name) \
+            .replace("GIT_BRANCH", "feature-initialization") \
+            .replace("UPSTREAM_CHANGES",UPSTREAM_CHANGES)
 
         JOB_CONFIG_TEST = profile.replace("GITLAB_URL", gitlab_url) \
             .replace("GROUP_ID", group_id) \
@@ -47,7 +54,8 @@ for jobs_name in jobs_arr:
             .replace("GOALS", GOALS) \
             .replace("SSH_CONFIG_NAME", "test") \
             .replace("SSH_CONFIG_COMMAND", "/home/test/leading_redeploy.sh " + jobs_name) \
-            .replace("GIT_BRANCH", "develop")
+            .replace("GIT_BRANCH", "develop") \
+            .replace("UPSTREAM_CHANGES",UPSTREAM_CHANGES)
 
         server.create_job("235_" + jobs_name, JOB_CONFIG_235)
         server.create_job("DEV_" + jobs_name, JOB_CONFIG_DEV)
@@ -55,22 +63,26 @@ for jobs_name in jobs_arr:
     else:
         with open('config.xml') as f:
             profile = f.read()
-        GOALS = 'clean package docker:build -Dmaven.test.skip=true'
+        GOALS = 'clean package docker:build -Dmaven.test.skip=true -U'
         JOB_CONFIG_235 = profile.replace("GITLAB_URL", gitlab_url) \
             .replace("GROUP_ID", group_id) \
             .replace("JOB_NAME", jobs_name) \
             .replace("GOALS", GOALS) \
             .replace("SSH_CONFIG_NAME", "235") \
             .replace("SSH_CONFIG_COMMAND", "/home/leading/leading_redeploy.sh " + jobs_name) \
-            .replace("GIT_BRANCH", "feature-initialization")
+            .replace("GIT_BRANCH", "feature-initialization") \
+            .replace("UPSTREAM_CHANGES",UPSTREAM_CHANGES) \
+            .replace("TIME_OUT","1000000000")
 
         JOB_CONFIG_DEV = profile.replace("GITLAB_URL", gitlab_url) \
             .replace("GROUP_ID", group_id) \
             .replace("JOB_NAME", jobs_name) \
             .replace("GOALS", GOALS) \
             .replace("SSH_CONFIG_NAME", "dev") \
-            .replace("SSH_CONFIG_COMMAND", "/home/scm/leading_redeploy.sh " + jobs_name) \
-            .replace("GIT_BRANCH", "feature-initialization")
+            .replace("SSH_CONFIG_COMMAND", "/home/dev/leading_redeploy.sh " + jobs_name) \
+            .replace("GIT_BRANCH", "feature-initialization") \
+            .replace("UPSTREAM_CHANGES",UPSTREAM_CHANGES) \
+            .replace("TIME_OUT","120000")
 
         JOB_CONFIG_TEST = profile.replace("GITLAB_URL", gitlab_url) \
             .replace("GROUP_ID", group_id) \
@@ -78,7 +90,9 @@ for jobs_name in jobs_arr:
             .replace("GOALS", GOALS) \
             .replace("SSH_CONFIG_NAME", "test") \
             .replace("SSH_CONFIG_COMMAND", "/home/test/leading_redeploy.sh " + jobs_name) \
-            .replace("GIT_BRANCH", "develop")
+            .replace("GIT_BRANCH", "develop") \
+            .replace("UPSTREAM_CHANGES",UPSTREAM_CHANGES) \
+            .replace("TIME_OUT","120000")
 
         server.create_job("235_" + jobs_name, JOB_CONFIG_235)
         server.create_job("DEV_" + jobs_name, JOB_CONFIG_DEV)
--- deleteJenkins.py
+++ deleteJenkins.py
@@ -2,12 +2,18 @@
 import jenkins
 
 # 获取jenkins连接
-server = jenkins.Jenkins('http://jenkins.66123123.com', username='', password='')
+server = jenkins.Jenkins('http://jenkins.66123123.com', username='leading2018', password='leading2018')
 
 jobs = server.get_all_jobs()
 
 jobs_str = '['
 for job in jobs:
-    jobs_str = jobs_str + '\'' + job['name'] + '\','
-    del_job = server.delete_job(job['name'])
+	if 'front' in job['name']:
+		continue
+	
+	jobs_str = jobs_str + '\'' + job['name'] + '\','
+	del_job = server.delete_job(job['name'])
+
 jobs_str = jobs_str[:-1] + ']'
+
+# del_job = server.delete_job('DEV_leading-erp-frontend')
